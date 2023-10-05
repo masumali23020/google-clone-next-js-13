992e2da1e5a846a92c3c73e633014fd1bda08423
masumali23020 <mdmasum0218@.com>
2023-10-05 11:31:38 +0600

    fetch data from google api and show the title

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,8 +1,18 @@
 
-const page = () => {
-  return (
-    <div>web page</div>
-  )
+const  WebSearchPage = async ({searchParams }) => {
+
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`)
+
+  const data = await response.json();
+  const results = data.items
+ 
+
+ return( <>
+  {results && results.map(result => (
+    <h1> {result.title}</h1>
+  ))}
+ </>)
+ 
 }
 
-export default page
+export default WebSearchPage
