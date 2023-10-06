4446e3bbf2e30987c43022c7fb9faa3cb99ed993
masumali23020 <mdmasum0218@.com>
2023-10-06 10:53:59 +0600

    loading effect image and web component

--- src/app/search/web/loading.js
+++ src/app/search/web/loading.js
@@ -2,7 +2,7 @@
 const loading = () => {
   return (
     <>
-    <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+    <div className="text-blue-400 mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
         <div className="h-2.5 w-48 bg-gray-200 rounded-full mb-2.5"></div>
         <div className="h-3.5 max-w-[360px] bg-gray-200 rounded-full mb-2.5"></div>
         <div className="h-2 max-w-[560px] bg-gray-200 rounded-full mb-2.5"></div>
