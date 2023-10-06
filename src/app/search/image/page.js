a0707e667786fa84f51729f01f9f6e4b5a4c7b94
masumali23020 <mdmasum0218@.com>
2023-10-06 11:11:37 +0600

    fix the vercel  bug

--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -1,10 +1,12 @@
+export const dynamic = "force-dynamic";
+
 
 import ImageSearchResults from "@/components/ImageSearchResults";
 import Link from "next/link";
 
  const ImageSearchPage = async({ searchParams }) => {
   const startIndex = searchParams.start || "1";
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&searchType=image&start=${startIndex}`
   );
